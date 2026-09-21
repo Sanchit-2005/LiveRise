2022077ff452c463370e0ea5302ae3541662634f
Sanchit Wakpanjar <vijaywakpanjar877@gmail.com>
2026-09-21 22:30:07 +0530

    updated the authentication styling

--- frontend/src/pages/authentication.jsx
+++ frontend/src/pages/authentication.jsx
@@ -16,7 +16,8 @@ import { useState } from "react";
 import { Authcontext } from "../contexts/Authcontext";
 import { useContext } from "react";
 import Snackbar from "@mui/material/Snackbar";
-
+import backgroundImage from "../utils/authy.jpg";
+import LockPersonIcon from "@mui/icons-material/LockPerson";
 const defaultTheme = createTheme();
 
 export default function Authentication() {
@@ -59,82 +60,102 @@ export default function Authentication() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Grid container component="main" sx={{ height: "100vh" }}>
-        <CssBaseline />
-        <Grid
-          item
-          xs={false}
-          sm={4}
-          md={7}
+      <Box
+        sx={{
+          minHeight: "100vh",
+          width: "100%",
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+
+          position: "relative",
+
+          "&::before": {
+            content: '""',
+            position: "absolute",
+            inset: 0,
+            backgroundColor: "rgba(0, 0, 0, 0.25)",
+          },
+        }}
+      >
+        <Paper
+          elevation={10}
           sx={{
-            backgroundImage:
-              "url(https://source.unsplash.com/random?wallpapers)",
-            backgroundRepeat: "no-repeat",
-            backgroundColor: (t) =>
-              t.palette.mode === "light"
-                ? t.palette.grey[50]
-                : t.palette.grey[900],
-            backgroundSize: "cover",
-            backgroundPosition: "center",
+            width: "420px",
+            maxWidth: "90%",
+            borderRadius: 1,
+            position: "relative",
+            zIndex: 1,
+            overflow: "hidden",
+            minHeight: "90%",
           }}
-        />
-        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        >
           <Box
             sx={{
-              my: 8,
-              mx: 4,
+              p: 4,
               display: "flex",
               flexDirection: "column",
               alignItems: "center",
             }}
           >
-            <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-              <LockOutlinedIcon />
+            <Avatar
+              sx={{
+                m: 1,
+                bgcolor: " #1de9b6",
+              }}
+            >
+              <LockPersonIcon style={{ color: "black" }} />
             </Avatar>
+            <br />
+
             <div>
               <Button
                 variant={formState === 0 ? "contained" : ""}
-                onClick={() => {
-                  setFormState(0);
-                }}
+                onClick={() => setFormState(0)}
+                color="warning"
               >
-                lOGIN{" "}
+                LOGIN
               </Button>
+
               <Button
                 variant={formState === 1 ? "contained" : ""}
-                onClick={() => {
-                  setFormState(1);
-                }}
+                onClick={() => setFormState(1)}
+                color="warning"
               >
-                SINGUP{" "}
+                SIGNUP
               </Button>
             </div>
 
-            <Box component="form" noValidate sx={{ mt: 1 }}>
-              {formState === 1 ? (
+            <Box
+              component="form"
+              noValidate
+              sx={{
+                mt: 1,
+                width: "100%",
+              }}
+            >
+              {formState === 1 && (
                 <TextField
                   margin="normal"
                   required
                   fullWidth
-                  id="fullname"
-                  label="fullname"
-                  name="fullname"
-                  autoComplete="fullname"
-                  autoFocus
+                  label="Full Name"
                   onChange={(e) => {
                     setName(e.target.value);
                   }}
                 />
-              ) : null}
+              )}
+
               <TextField
                 margin="normal"
                 required
                 fullWidth
-                id="username"
-                label="username"
-                name="usrname"
-                autoComplete="username"
-                autoFocus
+                label="Username"
                 onChange={(e) => {
                   setUsername(e.target.value);
                 }}
@@ -144,11 +165,8 @@ export default function Authentication() {
                 margin="normal"
                 required
                 fullWidth
-                name="password"
                 label="Password"
                 type="password"
-                id="password"
-                autoComplete="current-password"
                 onChange={(e) => {
                   setPassword(e.target.value);
                 }}
@@ -158,15 +176,21 @@ export default function Authentication() {
                 type="button"
                 fullWidth
                 variant="contained"
-                sx={{ mt: 3, mb: 2 }}
+                sx={{
+                  mt: 3,
+                  mb: 2,
+                  py: 1.3,
+                }}
+                style={{ backgroundColor: "#1c54b2" }}
                 onClick={handleAuth}
               >
                 {formState === 0 ? "Login" : "Register"}
               </Button>
             </Box>
           </Box>
-        </Grid>
-      </Grid>
+        </Paper>
+      </Box>
+
       <Snackbar
         open={open}
         autoHideDuration={4000}
